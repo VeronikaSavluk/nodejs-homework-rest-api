4915a46d816d190f972efb1d19b7d06a3a05c2fd
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-01-19 00:02:13 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const {middlewares: srvc} = require('../../service');
 
 const router = express.Router();
 
+router.get('/verify/:verificationToken', ctrl.verificationToken);
+router.post('/verify', ctrl.resendingMsg);
 router.get('/current', srvc.auth, ctrl.getCurrent);
 router.patch('/', srvc.auth, ctrl.updateSubscription);
 router.patch('/avatars', srvc.auth, srvc.upload.single('avatar'), ctrl.uploadAvatar);
